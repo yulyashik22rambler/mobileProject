e157fc3d67b2b362df080d43f5df9c9da6a509d3
julia <yulyashik@rambler.ru>
2023-07-01 20:10:31 +0700

    Add first mobile wiki test.

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -30,6 +34,24 @@ public class FirstTest {
 
     @Test
     public void firstTest() {
+        WebElement elementSkip = driver.findElementByXPath("//*[@text='Skip']");
+        elementSkip.click();
+        WebElement elementToInitSearch = waitForElementPresentByXpath("//*[contains(@text,'Search')]");
+        elementToInitSearch.click();
+        WebElement elementToEnterSearchLine = waitForElementPresentByXpath("//*[contains(@text,'Search')]");
+        elementToEnterSearchLine.sendKeys("Appium");
         System.out.println("First run test");
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String errorMessage, long timeInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath) {
+
+        return waitForElementPresentByXpath(xpath, "Can not find element", 10);
     }
 }
