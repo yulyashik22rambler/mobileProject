b3f02244868d2324a1c2396d15e811aef3a94ced
julia <yulyashik@rambler.ru>
2023-08-01 18:19:46 +0700

    Добавила недостающую часть теста

--- src/main/java/Ex5DeleteSavedSecondArticleTest.java
+++ src/main/java/Ex5DeleteSavedSecondArticleTest.java
@@ -1,5 +1,7 @@
 import lib.CoreTestCase;
 import lib.ui.ArticlePageObject;
+import lib.ui.NavigationUi;
+import lib.ui.SavedListPageObject;
 import lib.ui.SearchPageObject;
 import org.junit.Test;
 
@@ -26,5 +28,19 @@ public class Ex5DeleteSavedSecondArticleTest extends CoreTestCase {
         String articleTitle2 = articlePageObject2.getArticleTitle("Kotlin");
         assertEquals("We see unexpected title", "Kotlin (programming language)", articleTitle2);
         articlePageObject2.addToSaveList();
+
+        //2. Удаляет одну из статей
+        NavigationUi navigationUi = new NavigationUi(driver);
+        navigationUi.goToSavedList();
+        SavedListPageObject savedListPageObject = new SavedListPageObject(driver);
+        savedListPageObject.swipeToLeft("Java (programming language)");
+
+        //3. Убеждается, что вторая осталась
+        savedListPageObject.checkSavedArticleTitleIsPresent("Kotlin (programming language)");
+        savedListPageObject.checkSavedArticlesCount("1 of 1 article available offline");
+
+        //4. Переходит в неё и убеждается, что title совпадает
+        ArticlePageObject articlePageObject3 = savedListPageObject.goToArticlePageClickByTitle("Kotlin (programming language)");
+        assertEquals("We see unexpected title", "Kotlin (programming language)", articlePageObject3.getArticleTitle("Kotlin"));
     }
 }
